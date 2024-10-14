6a1c0e9263c8612d084fd751895f142db1e13b35
Nick Elgar <elgar89@icloud.com>
2024-10-14 10:48:07 +0100

    Added CREATE TABLE for Normalisation & Denormalisation training for Hannah.

---
+++ Type/DDL/CREATE TABLE - Normalisation and Denormalisation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Origin
+( 
+	 [Name] Varchar(100)
+	,[Address 1] Varchar(100)
+	,[Address 2] Varchar(100)
+)
+;
+
+INSERT INTO Origin (Name, [Address 1], [Address 2]) VALUES
+	('Nick', 'Cherry Tree', 'Hull'),
+	('Dave', 'Apple Tree', 'Leeds'),
+	('Bob', 'Monkey Tree', 'York'),
+	('Barry', 'Cherry Tree', 'Hull'),
+	('Claire', 'Apple Tree', 'Leeds'),
+	('Milton', 'Monkey Tree', 'York')
