ad3edcbcc61318ef8e143038157b417863f73d1f
Nick Elgar <elgar89@icloud.com>
2024-10-14 13:47:38 +0100

    further work

--- Type/DDL/CREATE TABLE PART 2 - Normalisation and Denormalisation.sql
+++ Type/DDL/CREATE TABLE PART 2 - Normalisation and Denormalisation.sql
@@ -1,14 +1,16 @@
 CREATE TABLE Normalised_Origin_People
 (
-	 [People_ID] INT PRIMARY KEY
+	 [People_ID] INT PRIMARY KEY NOT NULL IDENTITY(1,1)
 	,[Name] Varchar(100)
 )
 ;
 
 CREATE TABLE Normalised_Origin_Address
 (
-	 [Address_ID] INT PRIMARY KEY
+	 [Address_ID] INT PRIMARY KEY IDENTITY(1,1)
 	,[People_ID] INT FOREIGN KEY REFERENCES Normalised_Origin_People(People_ID)
 	,[Address_1] Varchar(100)
 	,[Address_2] Varchar(100)
 )
+
+DROP TABLE Normalised_Origin_Address, Normalised_Origin_People
